25bf0e535ac6e8d569c09866b12e788456cf3a52
Sai Teja Alanthrawar <saitejaalanthrawar@gmail.com>
2025-01-18 12:25:03 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import TalentSearchComponent from './TalentSearch.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
     <TalentSearchComponent />
   </StrictMode>,
 )
